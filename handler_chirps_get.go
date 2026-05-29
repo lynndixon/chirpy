9a150c25934851425a2302af786ded8d53b5c428
lynndixon <lynn@redhat.com>
2026-05-29 10:59:54 -0400

    CH5-L11 Get chirp by ID

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
@@ -25,4 +27,27 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, respChirps)
+}
+
+func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
+	chirpIDString := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
+		return
+	}
+
+	chirp, err := cfg.db.GetChirp(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get chirp", err)
+		return
+	}
+	respChirp := Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
+	}
+	respondWithJSON(w, http.StatusOK, respChirp)
 }
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -16,3 +16,8 @@ DELETE FROM chirps;
 SELECT *
 FROM chirps
 ORDER BY created_at ASC;
+
+-- name: GetChirp :one
+SELECT *
+FROM chirps
+WHERE id = $1;
