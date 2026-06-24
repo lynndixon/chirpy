2d4806493e1ff0f253cb28a38a7a5d5869d5cfd2
lynndixon <lynn@redhat.com>
2026-06-23 23:29:44 -0500

    CH9-L1 Get Chirps by author

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -7,6 +7,36 @@ import (
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err := uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
+		}
+
+		chirps, err := cfg.db.GetChirpsByAuthor(r.Context(), authorID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps by author", err)
+			return
+		}
+
+		respChirps := []Chirp{}
+		for _, chirp := range chirps {
+			respChirp := Chirp{
+				ID:        chirp.ID,
+				CreatedAt: chirp.CreatedAt,
+				UpdatedAt: chirp.UpdatedAt,
+				Body:      chirp.Body,
+				UserID:    chirp.UserID,
+			}
+			respChirps = append(respChirps, respChirp)
+		}
+
+		respondWithJSON(w, http.StatusOK, respChirps)
+		return
+	}
+
 	chirps, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps", err)
